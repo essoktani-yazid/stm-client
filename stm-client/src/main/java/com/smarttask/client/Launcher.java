e7d91c8004a1a84c3832e4f657bb5168ba8bcc87
ESSOKTANI Yazid <essoktani.yazid@gmail.com>
2026-02-12 20:05:31 +0100

    test

---
+++ stm-client/src/main/java/com/smarttask/client/Launcher.java
@@ -0,0 +1,8 @@
+package com.smarttask.client;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Appelle votre vraie classe principale
+        App.main(args);
+    }
+}
